dd409f478dc15466cd7d04ee65485f540117f2cf
spart <pochini.enrico@Studenti.gobettivolta.edu.it>
2024-02-17 12:32:46 +0100

    ora manda la roba su rete

--- app/src/main/java/com/example/walkaboutguide/MainActivity.java
+++ app/src/main/java/com/example/walkaboutguide/MainActivity.java
@@ -10,11 +10,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    Button login_button;
-    EditText input_password;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-    ThreadLogin threadLogin;
+public class MainActivity extends AppCompatActivity {
+
+    //componenti grafici
+    protected Button login_button;
+    protected EditText input_password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +34,25 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        login_button.setOnClickListener(new View.OnClickListener(){
+        login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    threadLogin = new ThreadLogin(input_password.getText().toString());
-                    threadLogin.start();
-                    if(threadLogin.getNominativo().equals("Errore")){
-                        Toast.makeText(MainActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
-                    }else{
-                        Toast.makeText(MainActivity.this, "Login succesful", Toast.LENGTH_SHORT).show();
-                        openMainMenu();
-                    }
+                ThreadLogin threadLogin = new ThreadLogin(input_password.getText().toString());
+                threadLogin.start();
+
+
+                if(input_password.getText().toString().equals("Errore")){
+
+                    Toast.makeText(MainActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+
+                }//if
+                else{
+
+                    Toast.makeText(MainActivity.this, "Login completato", Toast.LENGTH_SHORT).show();
+
+                    openMainMenu();
+
+                }//else
 
 
             }
@@ -46,11 +61,65 @@ public class MainActivity extends AppCompatActivity {
 
     //region Metodi Ausiliari
 
-    public void openMainMenu(){
+    public void openMainMenu() {
         Intent intent = new Intent(this, MainMenu.class);
         startActivity(intent);
     }
     //endregion
+
+
+
+    class ThreadLogin extends Thread {
+
+        //connessione
+        private int portaLogin = 149;
+        private String ipServer = "10.0.2.2";
+
+        //login
+        private String inputPassword;
+
+        public ThreadLogin(String inputPassword) {
+            this.inputPassword=inputPassword;
+        }//costruttore
+
+
+        @Override
+        public void run() {
+
+            login();
+
+        }
+
+
+        //manda la password al server che irotrna il nome della guida oppure errore se c'é un errore
+        public String login() {
+
+            String guida = "Errore";
+
+            try {
+
+                Socket socket = new Socket(ipServer, portaLogin);
+
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                out.println(inputPassword);
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                guida = in.readLine();
+
+                input_password.setText(guida);
+
+            } catch (IOException ignored) {
+
+
+            }
+
+            return guida;
+
+        }
+
+    }
+
+
 
 }
 
